088c033e597004908debbf48a1d628ae630ded05
Joel Fickson <joel.fickson@scalingfunds.com>
2022-05-24 11:03:40 +0200

    chore: ran 'added code styling and dependencies'

---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+const HomePage = () => {
+	return (
+		<div>
+			<h1>Welcome to the revolution.</h1>
+			<h4>This is the beginning of something cool!</h4>
+			<h4>A community driven revolution.</h4>
+		</div>
+	);
+};
+
+export default HomePage;
